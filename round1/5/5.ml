e90b533cdcfe3258be0b65fb9656ac55f55f16c3
David <david.rigaux@eisti.fr>
2016-11-04 18:32:01 +0100

    15 pts Round 3

--- round1/5/5.ml
+++ round1/5/5.ml
@@ -1,65 +1,30 @@
-(*
- * nom : jam
- * pre-conditions : fin,fout  2 noms de fichiers
- * pre-conditions : fonction algolec (unit_channel -> `a),
- *                  qui lit le contenu d'un cas de fin,
- * pre-conditions : fonction algopp (`a -> string),
- *                  qui renvoie la solution du cas passé en paramêtre
- * post-conditions : retourne le type unit (rien) après avoir
- *                   écrit le résultat de l'exécution de l'algopb sur tous
- *                   les cas de fin dans fout
- * auteur : EISTI
- * date création : 07/10/15
- *)
-
-let rec jam_rec flec algolec algopb nblignes total = match nblignes with
- 0 -> ""
-|n -> let line = (algolec flec) in
-	"Case #"^(string_of_int(total-nblignes+1))^": "^(algopb line)^"\n"^(jam_rec flec algolec algopb (nblignes-1) total) ;;
-
-let jam fin fout algolec algopb =
-	let fecr = open_out fout in
-	let flec = open_in fin in
-	let nb = (int_of_string(input_line flec)) in
-	begin
-		output_string fecr (jam_rec flec algolec algopb nb nb);
-		close_in flec;
-		close_out fecr;
-	end;;
-
-let lecture f =
-	input_line f;;
-
 #load "str.cma" ;;
 
-let extract_param s = List.map int_of_string(Str.split (Str.regexp " ") s);;
+let separe s = List.map int_of_string(Str.split (Str.regexp " ") s) 
 
 let lisse d b = if b > d then d else b;; (* si le nombre d'oeufs cassés est supérieur au nbre de lancés cela équivaut a d = b*)
 
-let listesauv = ref [];;
+let  listesauv = ref [];;
 
 let sauvegarde d b =
 	let rec save d b l  = match l with
-							 []-> None
-							 |[x;y;z] when x=d && y=b-> Some z
-							 |x::y::z::r -> save d b r
-							 |_ -> failwith "Erreur" in
-	save d b !listesauv;;
-
+                             []-> None
+                             |x::y::z::r when x=d && y=b-> Some z
+                             |x::y::z::r -> save d b r in
+                             save d b !listesauv;;
 let rec etage d b =
-	if (d >= 32) && (b >=32) then -1 else
+	if (d >= 32) && (b >=32) then -1 else 
 	if b = 1 then d else
 	if d = 1 then 1 else
-	(*if b = d then 2**b. -. 1. else *)
 
-					let x1 = match sauvegarde (d-1) (b-1) with
-								None -> etage(d-1) (b-1)
-								| Some z -> z in
-					let x2= match sauvegarde (d-1) b  with
-								None -> etage(d-1) b
-								|Some z -> z in
-					listesauv :=d::b::(x1+x2+1)::!listesauv;
-					(x1+x2+1);;
+                    let x1 = match sauvegarde (d-1) (b-1)    with
+                                None -> etage(d-1) (b-1)
+                              | Some z -> z in
+                    let x2= match sauvegarde (d-1) b  with
+                              None -> etage(d-1) b
+                              |Some z -> z in
+                     listesauv :=d::b::(x1+x2+1)::!listesauv;
+                     (x1+x2+1);;
 
 
 let rec drop f b x = if (etage x b )>= f then x else drop f b (x+1);;
@@ -73,7 +38,7 @@ let fin x = match x with
 	|f::d::b::[]-> string_of_int(etage d (lisse d b))^" "^string_of_int(drop f b 1)^" "^string_of_int(break f d 1)
 	|_ -> failwith "Erreur";;
 
-let resolve a = fin  (extract_param a);;
+let resolve x = fin  (separe x);;
 
-(* Exemple d'utilisation *)
-(* jam "C-large-practice.in" "toto.out" lecture resolve;;*)
+
+(*a = f*)
---
+++ round3/5/5.ml
@@ -0,0 +1,54 @@
+(*
+ * nom : jam
+ * pre-conditions : fin,fout  2 noms de fichiers
+ * pre-conditions : fonction algolec (unit_channel -> `a),
+ *                  qui lit le contenu d'un cas de fin,
+ * pre-conditions : fonction algopp (`a -> string),
+ *                  qui renvoie la solution du cas passé en paramêtre
+ * post-conditions : retourne le type unit (rien) après avoir
+ *                   écrit le résultat de l'exécution de l'algopb sur tous
+ *                   les cas de fin dans fout
+ * auteur : EISTI
+ * date création : 07/10/15
+ *)
+
+let rec jam_rec flec algolec algopb nblignes total = match nblignes with
+ 0 -> ""
+|n -> let line = (algolec flec) in
+	"Case #"^(string_of_int(total-nblignes+1))^": "^(algopb line)^"\n"^(jam_rec flec algolec algopb (nblignes-1) total) ;;
+
+let jam fin fout algolec algopb =
+	let fecr = open_out fout in
+	let flec = open_in fin in
+	let nb = (int_of_string(input_line flec)) in
+	begin
+		output_string fecr (jam_rec flec algolec algopb  nb nb);
+		close_in flec;
+		close_out fecr;
+	end;;
+#load "str.cma";;
+
+let lecture f =
+	let a = input_line f in
+	a;;
+
+let rec jamLoop n fich i  lb la =
+	(*ici mettre les fonctions d'Olivier*)
+	let ligne = input_line fich
+	if i = n then
+		if la = [] then "GOOD" else "BAD"
+	(*remplacer lb et la par les fonctions d'olivier, les appeller*)
+	else jamLoop n fich i+1 lb la;;
+
+let jamEval a =
+	let lb = [] in
+	let la = [] in
+	let fich = "C-small-practice.in" in
+	let n = string_of_int (input_line fich) in
+	jamLoop n fich 0 lb la;;
+
+
+
+
+(* Exemple d'utilisation *)
+(* jam "C-small-practice.in" "toto.out" lecture jamMountain;;*)
